582570b1b3c2977e42e71202e228011df1f069bd
Abbie Donaldson <donaldso@roe.ac.uk>
2021-10-06 17:41:14 +0200

    New script for uncertainty from colours

--- LSST_baselinev13_query.py
+++ LSST_baselinev13_query.py
@@ -3,7 +3,7 @@ import os
 home_path = os.path.expanduser('~/')
 path_67p = home_path+'Documents/year1/shape_modelling/67p/'
 
-conn = sqlite3.connect('baseline_v1.3_10yrs.db')
+conn = sqlite3.connect(path_67p+'baseline_v1.3_10yrs.db')
 cur = conn.cursor()
 
 print('\nColumns in summaryAllProps table:')
@@ -11,18 +11,17 @@ data=cur.execute('''SELECT * FROM SummaryAllProps''')
 for column in data.description:
     print(column[0])
 
-
 obs_ids = []
 file = open(path_67p+'LSST_out_LC_67p.txt')
 lines = file.readlines()
 for line in lines[25:]:
-    fata = line.split()
-    obs_ids.append(str(fata[1]))
+    dat = line.split()
+    obs_ids.append(str(dat[1]))
 print(len(obs_ids))
 # Display data
 
 #string+=observationId+","
-searchString="select fiveSigmaDepth, observationId from summaryAllProps where observationId in ("
+searchString="select observationId, filter, fiveSigmaDepth from summaryAllProps where observationId in ("
 
 for item in obs_ids:
     searchString+=item
@@ -34,9 +33,10 @@ for item in obs_ids:
 print('\nData in summaryAllProps table:')
 data = cur.execute(searchString)
 #data=cur.execute('''SELECT fiveSigmaDepth, observationId FROM SummaryAllProps where observationId=''')
+fout=open(path_67p+'LSST_SQL_output.txt', 'w+')
 for row in data:
-    print(row)
+    #print(row)
+    fout.write(str(row)+'\n')
 
-
-
+fout.close()
 conn.close()
